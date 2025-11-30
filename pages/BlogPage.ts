4354981a3d80add8d72cedc674877d1212513f5f
Ayanda Khaka <khakalihle@gmail.com>
2025-11-30 12:07:15 +0200

    modified yaml

--- pages/BlogPage.ts
+++ pages/BlogPage.ts
@@ -18,60 +18,73 @@ export default class ChangePassword {
     private yourNameInput = '#input-name';
     private yourEmailInput = '#input-email';
     private yourCommentInput = '#input-comment';
-    private nameErrorMessage = '.invalid-feedback';
+    private nameErrorMessage = '.invalid-feedback';   
     private emailErrorMessage = '.invalid-feedback';
     private commentErrorMessage = '.invalid-feedback';
+    private blogLinkLocator = 'span.title';
 
 
     // ------------------------------
     async clickBlogLink() {
 
-        const blogLink = this.page.locator('span.title', { hasText: "Blog" }).nth(1);
-        await this.actionHelper.click(blogLink, "Click Blog Link");
+        const blogLink = this.page.locator(this.blogLinkLocator, { hasText: "Blog" }).nth(1);
+        await this.actionHelper.click(
+            blogLink, "Click Blog Link");
     }
 
     async verifyNameErrorMessage(expectedMessage: string) {
-        const nameErrorMessage = this.page.locator('.invalid-feedback').nth(0);
-        await this.actionHelper.verifyText(nameErrorMessage, expectedMessage, 'Name Error Message');
+        const nameErrorMessage = this.page.locator(this.nameErrorMessage).nth(0);
+        await this.actionHelper.verifyText(
+            nameErrorMessage, expectedMessage, 'Name Error Message');
     }
 
     async verifyEmailErrorMessage(expectedMessage: string) {
-        const emailErrorMessage = this.page.locator('.invalid-feedback').nth(1);
+        const emailErrorMessage = this.page.locator(this.emailErrorMessage).nth(1);
         await this.actionHelper.verifyText(emailErrorMessage, expectedMessage, 'Email Error Message');
     }
 
     async verifyCommentErrorMessage(expectedMessage: string) {
-        const commentErrorMessage = this.page.locator('.invalid-feedback').nth(2);
-        await this.actionHelper.verifyText(commentErrorMessage, expectedMessage, 'Comment Error Message');
+        const commentErrorMessage = this.page.locator(this.commentErrorMessage).nth(2);
+        await this.actionHelper.verifyText(
+            commentErrorMessage, expectedMessage, 'Comment Error Message');
     }
 
     async fillYourName(name: string) {
-        await this.actionHelper.type(this.yourNameInput, name, "Your Name Input Field");
+        await this.actionHelper.type(
+            this.yourNameInput, name, "Your Name Input Field");
     }
 
     async fillYourEmail(email: string) {
-        await this.actionHelper.type(this.yourEmailInput, email, "Your Email Input Field");
+        await this.actionHelper.type(
+            this.yourEmailInput, email, "Your Email Input Field");
     }
 
     async fillYourComment(comment: string) {
-        await this.actionHelper.type(this.yourCommentInput, comment, "Your Comment Input Field");
+        await this.actionHelper.type(
+            this.yourCommentInput, comment, "Your Comment Input Field");
     }
 
     async scrollToLaptopImage() {
-        await this.page.getByAltText("amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus").first().scrollIntoViewIfNeeded();
+        await this.page.getByAltText(
+            "amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus").
+            first().scrollIntoViewIfNeeded();
     }
 
     async clickLaptopImage() {
-        await this.page.getByAltText("amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus").first().click();
+        await this.page.getByAltText(
+            "amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus")
+            .first().click();
     }
 
 
     async fillCommentDetails(comment: string) {
-        await this.actionHelper.type(this.commentInput, comment, "Comment Input Field");
+        await this.actionHelper.type(
+            this.commentInput, comment, "Comment Input Field");
     }
 
     async clickPostCommentButton() {
-        await this.actionHelper.click(this.postCommentButton, 'Post Comment Button');
+        await this.actionHelper.click(
+            this.postCommentButton, 'Post Comment Button');
     }
 
 
@@ -81,9 +94,7 @@ export default class ChangePassword {
     }
 
     async verifyBlogCommentSuccessMessage(expectedMessage: string) {
-        await this.actionHelper.verifyText(`.${this.successText}`, expectedMessage, 'Blog Comment Success Message');
+        await this.actionHelper.verifyText(
+            `.${this.successText}`, expectedMessage, 'Blog Comment Success Message');
     }
-
-
-
 }
